514283e4a539f2aa151d8d60da777ce74f6e4ee4
Asyraaf Amran <asrafamran06@gmail.com>
2025-02-08 10:26:19 +0800

    initial commit for frontend

---
+++ frontend/ad-guard/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <div>First Page</div>
+    </HydrateClient>
+  );
+}
